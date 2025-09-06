271918dbfbca8a1cb3d890871c20823aab3cc91f
saicharan293 <2493saicharan@gmail.com>
2025-09-06 08:35:59 +0530

    started with DOM

---
+++ FourthShow.js
@@ -0,0 +1,5 @@
+//DOM : Document Object Model
+// oka web page teesukunte adi html tho basic ga build ayyi untadi,
+// ee web page anedi live vellinappudu, oka tree structure form chesukuntadi
+// andulo different tags undachu, different elements undachu, avanni nested ayyi untay
+
