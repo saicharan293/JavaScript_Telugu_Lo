44495fab888ad1c170dbd3a7b73d5116dc8311cf
saicharan293 <2493saicharan@gmail.com>
2025-09-06 18:00:42 +0530

    getElementById is going on

--- FourthShow.js
+++ FourthShow.js
@@ -3,3 +3,11 @@
 // ee web page anedi live vellinappudu, oka tree structure form chesukuntadi
 // andulo different tags undachu, different elements undachu, avanni nested ayyi untay
 
+//fetching the node by using Id
+
+// let headTag = document.querySelector("#headTag");
+// here, headTag will show the entire element completely as in the html page
+
+let headTag = document.getElementById("headTag")
+// here, headTag will show the tag name along with its id associated with it.
+console.log(headTag);
