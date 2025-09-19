f7ae089dae547dc9b938c1a3c288c92524c995d1
saicharan293 <2493saicharan@gmail.com>
2025-09-19 22:57:21 +0530

    LocalStorage started

---
+++ LocalAndSessionStorageCookies.js
@@ -0,0 +1,31 @@
+// Local storage ante, browser ki data base lantidi, 
+// ante okasari andulo data save cheste, 
+// browser close chesina kani, data delete avvadu
+
+// Session storage ante, browser lo idi temporary database lantidi, 
+// ante browser tab open lo unantha varaku, data andulo store chesi use cheyachu, 
+// but..... once tab or browser ni close cheste, Session close aipoddi
+
+// Cookies ante, browser lo idi oka property, indulo kuda data store cheyachu. 
+// Idi very light weight data store cheyataniki use chestamu, tab close chesina kuda data delete avvadu
+
+//------------------------------------------------------
+
+//Local Storage
+// Data ni store cheyali ante localStorage.setItem("--var-name","--data")
+
+localStorage.setItem("just","dat");
+
+// localStorage lo store aina data ni get cheyali ante localStorage.getItem("--var-name")
+
+let val=localStorage.getItem("just");
+console.log(val);
+
+// Local Storage lo store aina data ni delete cheyali ante localStorage.removeItem("--var-name")
+
+localStorage.removeItem("just");
+
+// Kani Local storage lo update cheyali anna kani, setItem ne use chestamu, endukante setItem lo same variable ki malli different value tho update cheste, ade update
+
+localStorage.setItem("just","dat");
+localStorage.setItem("just", "change")
